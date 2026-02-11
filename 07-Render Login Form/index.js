4ed4463e953edf48fc8c3e3e6f1f9eca12d665b3
Harsh28Pandey <harsh28.knp@gmail.com>
2026-02-11 22:15:14 +0530

    Update index.js

--- 07-Render Login Form/index.js
+++ 07-Render Login Form/index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = 8000
 
 app.use('/', (req, res) => {
-
+    res.send("<h1>Welcome to the home page</h1>")
 })
 
 app.use('/login', (req, res) => {
@@ -12,7 +12,7 @@ app.use('/login', (req, res) => {
 })
 
 app.use('/submit', (req, res) => {
-
+    res.send("<h1>Submit page</h1>")
 })
 
 app.listen(PORT, () => {
