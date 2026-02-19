fb193e224a72829f5e100b527e77fbf67a0a9fdc
Harsh28Pandey <harsh28.knp@gmail.com>
2026-02-19 22:22:16 +0530

    Update index.js

--- 12-Middleware (Router Level)/index.js
+++ 12-Middleware (Router Level)/index.js
@@ -2,3 +2,38 @@
 
 //todo Example - Shown below
 
+import express from "express"
+
+const app = express()
+const PORT = 8000
+
+//? middleware
+const customMiddlewareQuery = (req, res, next) => {
+    console.log("Query: ", req.query.age)
+    if (!req.query.age || req.query.age < 18) {
+        res.send("<h1>You are not allowed</h1>")
+    } else {
+        next()
+    }
+}
+
+//? routes
+app.get("/", (req, res) => {
+    res.send("<h1>Hello, Home!</h1>")
+})
+
+app.get("/login", customMiddlewareQuery, (req, res) => {
+    res.send("<h1>Hello, Login!</h1>")
+})
+
+app.get("/users", customMiddlewareQuery, (req, res) => {
+    res.send("<h1>Hello, Users!</h1>")
+})
+
+app.get("/products", (req, res) => {
+    res.send("<h1>Hello, Products!</h1>")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
