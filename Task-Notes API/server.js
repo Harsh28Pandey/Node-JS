b29bb1d9ea22b8dc0e202b6b4ea66d1c980fee69
Harsh28Pandey <harsh28.knp@gmail.com>
2026-03-02 10:53:29 +0530

    task-notes api

---
+++ Task-Notes API/server.js
@@ -0,0 +1,7 @@
+//? server ko start karna 
+
+const app = require("./src/app.js")
+
+app.listen(3000, () => {
+    console.log('Server is running on PORT 3000')
+})
---
+++ Task-Notes API/src/app.js
@@ -0,0 +1,18 @@
+//? server ko create karna 
+
+const express = require('express');
+
+const app = express()
+
+const notes = []
+
+module.exports = app
+
+
+/*
+    todo: Sample note object
+    note = {
+        title: 'Task name',
+        description: 'Task description'
+    }
+*/
