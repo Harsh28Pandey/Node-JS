ae7324f1fc1091cd0d0c99aab1aea7c3d5a89980
Harsh28Pandey <harsh28.knp@gmail.com>
2026-03-03 09:53:58 +0530

    notes api

--- Task-Notes API/src/app.js
+++ Task-Notes API/src/app.js
@@ -4,7 +4,49 @@ const express = require('express');
 
 const app = express()
 
+//? middleware to parse json data from request body
+app.use(express.json())
+
 const notes = []
+
+//? create a note
+app.post("/notes", (req, res) => {
+    // console.log(req.body)
+    notes.push(req.body)
+    res.status(201).json({
+        message: "Note created successfully",
+        success: true
+    })
+})
+
+//? get all notes
+app.get("/notes", (req, res) => {
+    res.status(200).json({
+        message: "All notes fetched successfully",
+        notes: notes,
+        success: true
+    })
+})
+
+//? delete a note
+app.delete("/notes/:id", (req, res) => {
+    const id = req.params.id
+    delete notes[id]
+    res.status(200).json({
+        message: "Note deleted successfully",
+        success: true
+    })
+})
+
+//? update a note
+app.patch("/notes/:id", (req, res) => {
+    const id = req.params.id
+    const description = req.body.description
+    notes[id].description = description
+    res.status(200).json({
+        message: "Note updated successfully"
+    })
+})
 
 module.exports = app
 
